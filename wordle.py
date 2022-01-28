21952b50c9f648ec5d6ae924fbc453d493cf8ac6
SpitKid <ryleymolloy@gmail.com>
2022-01-28 18:31:42 -0500

    Spacing

--- wordle.py
+++ wordle.py
@@ -9,17 +9,16 @@ import pygame.font
 wordfile = open("wordle-answers-alphabetical.txt", "r")
 words = wordfile.read()
 words = words.split('\n')
+wordfile.close()
 random_word = words[random.randint(0, len(words))]
 print(random_word)
 
 #initialize game
 pygame.init()
-screenWidth = 700
-screenHeight = 1000
-
 titleFont = pygame.font.Font(None, 100)
 guessFont = pygame.font.Font(None, 75)
 smallFont = pygame.font.Font(None, 32)
+
 YELLOW = (181, 159, 59)
 GREEN = (83, 141, 78)
 DARK_GRAY = (58, 58, 60)
@@ -29,9 +28,20 @@ BACKGROUND = (18, 18, 19)
 COLOR_INACTIVE = DARK_GRAY
 COLOR_ACTIVE = LIGHT_GRAY
 
-BOX_SIZE = 75
-INPUT_WIDTH = 500
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 1000
+GAME_WIDTH = 500
+GAME_X = (SCREEN_WIDTH - GAME_WIDTH) // 2
 
+BOX_SIZE = 85
+BOX_MARGIN = (GAME_WIDTH - 5 * BOX_SIZE) // 4
+
+WORDLE_Y = 30
+GRID_Y = WORDLE_Y + 90
+MSG_X = 100
+MSG_Y = 750
+ERROR_X = 100
+ERROR_Y = 900
 class letter_box:
     def __init__(self, 
                 screen, 
@@ -65,21 +75,20 @@ class letter_box:
 
 #start game
 def main():
-    screen = pygame.display.set_mode([screenWidth, screenHeight])
-    # Fill the background with black
+    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
     screen.fill(BACKGROUND)
     clock = pygame.time.Clock()
-    input_box = pygame.Rect((screenWidth - INPUT_WIDTH) // 2, 800, INPUT_WIDTH, 75)
+    input_box = pygame.Rect(GAME_X, 800, GAME_WIDTH, 75)
     COLOR = COLOR_INACTIVE
     active = False
     text = ''
     n = 0
 
-    wordletxt = titleFont.render("Wordle", True, (255,255,255))
-    wordError = smallFont.render("Please guess a valid 5-letter word", True, (255,255,255))
-    screen.blit(wordletxt, (100,20))
-    x_pos = np.arange(100, 600, 100)
-    y_pos = np.arange(100, 700, 100)
+    wordletxt = titleFont.render("Wordle", True, WHITE)
+    wordError = smallFont.render("Please guess a valid 5-letter word", True, WHITE)
+    screen.blit(wordletxt, (GAME_X, WORDLE_Y))
+    x_pos = np.linspace(GAME_X, GAME_X + 4 * (BOX_SIZE + BOX_MARGIN), 5)
+    y_pos = np.linspace(GRID_Y, GRID_Y + 5 * (BOX_SIZE + BOX_MARGIN), 6)
     boxes = np.empty((6, 5), dtype=object)
     for x in range(5):
         for y in range(6):
@@ -107,10 +116,10 @@ def main():
                         user_guess = text.lower()
                         if user_guess not in words:
                             #draw error text
-                            screen.blit(wordError, (100,900))
+                            screen.blit(wordError, (ERROR_X, ERROR_Y))
                         else:
                             #draw over error text
-                            pygame.draw.rect(screen, BACKGROUND, (100, 900, 500, 40))
+                            pygame.draw.rect(screen, BACKGROUND, (ERROR_X, ERROR_Y, GAME_WIDTH, 40))
                             for i in range(len(user_guess)):
                                 guess = user_guess[i]
                                 box = boxes[n][i]
@@ -119,7 +128,7 @@ def main():
                                     for k in range(i,5):
                                         if guess == user_guess[k] and user_guess[k] == random_word[k]:
                                             correctlettercount += 1
-                                            box.draw(GREEN)
+                                            boxes[n][k].draw(GREEN)
                                     if user_guess[0:i+1].count(guess) + correctlettercount <= random_word.count(user_guess[i]):
                                         box.draw(YELLOW)
                                 box.write(guess)
@@ -143,12 +152,11 @@ def main():
                     (input_box.x + (input_box.w - text_rect.width) // 2, 
                     input_box.y + (input_box.h - text_rect.height) // 2 + 3))
         #text above guessbox
-        guesstxt = smallFont.render("Guess word #" + str(n+1) + ":", True, WHITE)
-        pygame.draw.rect(screen, BACKGROUND, (100, 750, 500, 40))
-        screen.blit(guesstxt, (100,750))
+        msg = smallFont.render("Guess word #" + str(n+1) + ":", True, WHITE)
+        pygame.draw.rect(screen, BACKGROUND, (MSG_X, MSG_Y, GAME_WIDTH, 40))
+        screen.blit(msg, (MSG_X, MSG_Y))
         pygame.display.flip()
         clock.tick(30)
-
 
 # Done! Time to quit.
 if __name__ == '__main__':
